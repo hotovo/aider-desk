f2bdaf03748ad8819ca01e2d0fae39246a5cd962
Kristián Žiga <xziga@fi.muni.cz>
2025-06-25 12:13:23 +0200

    feat: changed links

--- src/main/progress-window.ts
+++ src/main/progress-window.ts
@@ -1,7 +1,6 @@
 import { join } from 'path';
 
 import { BrowserWindow, ipcMain } from 'electron';
-import { is } from '@electron-toolkit/utils';
 
 export class ProgressWindow {
   private window: BrowserWindow;
@@ -48,11 +47,7 @@ export class ProgressWindow {
       this.window.webContents.send('set-progress', progress);
     });
 
-    if (is.dev) {
-      void this.window.loadFile('src/renderer/progress.html');
-    } else {
-      void this.window.loadFile(join(__dirname, '../renderer/progress.html'));
-    }
+    void this.window.loadFile(join(__dirname, '../renderer/progress.html'));
   }
 
   on(event: 'ready', callback: () => void): void {
