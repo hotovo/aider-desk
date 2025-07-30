42f9646589fb0475039bd5f67caef9b0d76b5c95
Kristian Ziga <xziga@fi.muni.cz>
2025-07-30 11:23:24 +0200

    feat: added expandable section with nothing in it

--- src/renderer/src/components/settings/agent/providers/OpenRouterParameters.tsx
+++ src/renderer/src/components/settings/agent/providers/OpenRouterParameters.tsx
@@ -1,4 +1,4 @@
-import { ChangeEvent } from 'react';
+import { ChangeEvent, ReactNode } from 'react';
 import { useTranslation } from 'react-i18next';
 import { OpenRouterProvider } from '@common/agent';
 
@@ -6,6 +6,7 @@ import { ProviderModels } from './ProviderModels';
 
 import { Input } from '@/components/common/Input';
 import { useEffectiveEnvironmentVariable } from '@/hooks/useEffectiveEnvironmentVariable';
+import { Accordion } from '@/components/common/Accordion';
 
 type Props = {
   provider: OpenRouterProvider;
@@ -28,6 +29,18 @@ export const OpenRouterParameters = ({ provider, onChange }: Props) => {
     onChange({ ...provider, models: updatedModels });
   };
 
+  const renderSectionAccordion = (title: ReactNode, children: ReactNode, open?: boolean, setOpen?: (open: boolean) => void) => (
+    <Accordion
+      title={<div className="flex-1 text-left text-sm font-medium px-2">{title}</div>}
+      chevronPosition="right"
+      className="mb-2 border rounded-md border-neutral-700"
+      isOpen={open}
+      onOpenChange={setOpen}
+    >
+      <div className="p-4 pt-2">{children}</div>
+    </Accordion>
+  );
+
   return (
     <div className="space-y-2">
       <div className="!mt-0 !mb-5">
@@ -35,6 +48,7 @@ export const OpenRouterParameters = ({ provider, onChange }: Props) => {
           Get OpenRouter API key
         </a>
       </div>
+      {renderSectionAccordion(t('onboarding.providers.advancedSettings'), <div className="space-y-2"></div>)}
       <Input
         label={t('openRouter.apiKey')}
         type="password"
